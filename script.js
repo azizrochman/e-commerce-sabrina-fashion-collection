c13826f88f6263aecbfeac0a8b6da0b2ea866a6f
azizrochman <azrchmn23@gmail.com>
2025-01-17 10:45:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById('menu');
+const nav = document.getElementById('navbar-box');
+const close = document.getElementById('close');
+
+if (menu) {
+    menu.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
